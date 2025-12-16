79735ec2080069e6250608e8f9bb9c98769cbb0a
JoaoSantosEAU <2231131@my.ipleiria.pt>
2025-12-16 22:08:59 +0000

    Update Project_RC_SEEV.ino
    Reorganized code structure, updated comments, and ensured consistent formatting.

--- Project_RC_SEEV.ino
+++ Project_RC_SEEV.ino
@@ -270,8 +270,8 @@ void setup(){
 
 	// Iniciar o Sensor de Distancia:
 	if (!sensor.init()) {
-	  Serial.println("Failed to detect and initialize VL53L0X!");
-	  while (1) { vTaskDelay(1000 / portTICK_PERIOD_MS); }
+	  Serial.println("Failed to detect and initialize VL53L0X!");	
+	  while (1) { vTaskDelay(1000 / portTICK_PERIOD_MS); Serial.println("Por favor renicie!"); }
 	  }
 	  sensor.setTimeout(500);      // previne se o sensor bloqueie se falhar
 	  sensor.startContinuous(50);  // Mede a cada 50ms
@@ -403,6 +403,7 @@ void vTask_Sensor_Distancia(void *pvParameters) {
 	// Segurança caso o sensor não inicialize
 	while (!sensorInitialized) {
 	     vTaskDelay(10 / portTICK_PERIOD_MS);
+		 Serial.println("Por favor renicie o sensor de distância!");
 	}
 
 
@@ -698,4 +699,5 @@ void vTask_Bluetooth(void *pvParameters) {
 void loop(){
 	vTaskDelete(NULL);
 }
+
 
